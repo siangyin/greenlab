41f10f12829c008e6f5ef1c6086f44c70dd70ecc
Lee Siang Yin <siangyin916@gmail.com>
2022-02-16 21:30:29 +0800

    🏗 file setup & pkg installation

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+// import { MDBBtn, MDBContainer } from "mdb-react-ui-kit";
+
+function App() {
+	return (
+		<div>
+			<NavBar /> Hello
+		</div>
+	);
+}
+
+export default App;
