f4a02715bc2d63567c871a19ee7c357a295ab218
Lee Siang Yin <siangyin916@gmail.com>
2022-02-23 19:46:41 +0800

    on progress - cartpage updating

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -44,7 +44,7 @@ function CartPage() {
 				<div className="w-3/4 bg-white px-10 py-10">
 					<div className="flex justify-between border-b pb-8">
 						<h1 className="font-semibold text-2xl">Shopping Cart</h1>
-						<h2 className="font-semibold text-2xl">3 Items</h2>
+						<h2 className="font-semibold text-2xl"> {qtySum} Items</h2>
 					</div>
 					{/* header */}
 					<div className="flex mt-10 mb-5">
@@ -91,7 +91,7 @@ function CartPage() {
 					</h1>
 					<div className="flex justify-between mt-10 mb-5">
 						<span className="font-semibold text-sm uppercase">
-							Total Items {qtySum}
+							Items: {qtySum}
 						</span>
 						<span className="font-semibold text-sm">590$</span>
 					</div>
