545ca4df06cc34edd77ede3ddf66ecf78c351892
Lee Siang Yin <siangyin916@gmail.com>
2022-02-23 23:18:52 +0800

    Update ButtonAction.jsx

--- src/components/ButtonAction.jsx
+++ src/components/ButtonAction.jsx
@@ -2,13 +2,7 @@ import React from "react";
 
 const ButtonAction = ({ labelText, handleClick }) => {
 	return (
-		<button
-			type="button"
-			onClick={handleClick}
-			className="text-white bg-rose-400 hover:bg-rose-600 focus:ring-4 focus:ring-rose-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center capitalize"
-		>
-			{labelText}
-		</button>
+		
 	);
 };
 
