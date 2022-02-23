6d1548e0f87ae71561af137e7be4bbf331a1b788
Lee Siang Yin <siangyin916@gmail.com>
2022-02-23 19:57:10 +0800

    test to deploy

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -11,6 +11,7 @@ const sendGetRequest = async () => {
 };
 import { LoginContext, AdminContext, UserContext } from "./context";
 
-const BE_URL = `http://localhost:5000/api/v1`;
+const BE_URL =
+	`https://greenlab-be.herokuapp.com/api/v1` || `http://localhost:5000/api/v1`;
 
 export { LoginContext, AdminContext, UserContext, BE_URL };
