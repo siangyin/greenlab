bcd49941044fb72dcc917f43bbcdec12570f580f
Lee Siang Yin <siangyin916@gmail.com>
2022-02-25 14:16:42 +0800

    Update index.js

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 import { LoginContext, AdminContext, UserContext } from "./context";
 
-const BE_URL = process.env.BE_URL;
+const BE_URL = `https://greenlab-be.herokuapp.com/api/v1`;
 // `https://greenlab-be.herokuapp.com/api/v1`;
-
+console.log(BE_URL);
 export { LoginContext, AdminContext, UserContext, BE_URL };
